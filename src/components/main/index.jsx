1503447698aa5653ed52e3e0cc5401aa90cc44b7
Nam Le <letrinhhainam@gmail.com>
2019-10-29 21:29:24 +0700

    Add sample code for Hooks

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from 'logo.svg';
 import 'components/main/main.css';
 
+function useFetch(url) {
+  const [data, setDataState] = useState(null);
+  const [loading, setLoadingState] = useState(true);
+  useEffect(() => {
+    setLoadingState(true);
+    fetch(url)
+      .then(j => j.json())
+      .then(data => {
+        setDataState(data);
+        setLoadingState(false);
+      });
+  }, [url]);
+  return { data, loading };
+}
+
 function App() {
-  return (
+  const { data, loading } = useFetch('https://api.etilbudsavis.dk/v2/dealerfront?country_id=DK');
+
+  return loading ? (
+    'Loading!!!'
+  ) : (
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
